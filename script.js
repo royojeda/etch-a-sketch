0bd7730f9e207f09ef32f90756bda5786db96f21
Dennis Ojeda <royojeda1@gmail.com>
2022-04-27 22:29:09 +0800

    Add initial grid

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 const container = document.createElement('div');
-container.style['background-color'] = 'black'
+container.setAttribute('class', 'container');
 document.body.appendChild(container);
+
+for (let i = 0; i < 16; i++) {
+  const columnDiv = document.createElement('div');
+  columnDiv.setAttribute('class', 'column');
+  for (let j = 0; j < 16; j++) {
+    const rowDiv = document.createElement('div');
+    rowDiv.setAttribute('class', 'row');
+    columnDiv.appendChild(rowDiv);
+  }
+  container.appendChild(columnDiv);
+}
+
